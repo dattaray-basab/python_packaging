b448b51cf136e49885f0d5a60fd28a575510db05
Basab Dattaray <25799611+basab-dattaray@users.noreply.github.com>
2021-11-06 14:18:44 -0700

    1.2

--- IntermediatePkg/__init__.py
+++ IntermediatePkg/__init__.py
@@ -1,5 +1,6 @@
 
 from .P1.Module1 import P1_fn_1
 from .P1.P11.Module11 import P11_fn_11
+from IntermediatePkg.P1.P12.mgr_Module12 import Module12_mgr
 
 print('Inter')
--- call_2.py
+++ call_2.py
@@ -1,8 +1,9 @@
-from IntermediatePkg import P1_fn_1, P11_fn_11
+from IntermediatePkg import P1_fn_1, P11_fn_11, Module12_mgr
 
 
 P1_fn_1()
 P11_fn_11()
 
-
+P11_fn_12 = Module12_mgr()
+P11_fn_12()
 

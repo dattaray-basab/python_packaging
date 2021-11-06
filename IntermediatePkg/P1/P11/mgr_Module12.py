fc708b9d0e3ebba12117e37063d1a240aecdf289
Basab Dattaray <25799611+basab-dattaray@users.noreply.github.com>
2021-11-06 13:49:56 -0700

    1.1

---
+++ IntermediatePkg/P1/P11/mgr_Module12.py
@@ -0,0 +1,5 @@
+def Module12_mgr():
+    def P11_fn_12():
+        print('P11 fn_12')
+    return P11_fn_12
+
--- call_2.py
+++ call_2.py
@@ -1,5 +1,5 @@
-from IntermediatePkg import P1_fn_1
-from IntermediatePkg import P11_fn_11
+from IntermediatePkg import P1_fn_1, P11_fn_11
+
 
 P1_fn_1()
 P11_fn_11()
